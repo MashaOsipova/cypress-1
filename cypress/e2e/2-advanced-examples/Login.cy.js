53db806d12a1969cd2a390dd1837166c5131cda2
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-05-05 13:02:59 +0300

    init

--- cypress/e2e/2-advanced-examples/Login.cy.js
+++ cypress/e2e/2-advanced-examples/Login.cy.js
@@ -1,30 +1,32 @@
+describe("login process", () => {
 beforeEach(() => {
-  cy.viewport(Cypress.env("viewportWidth"), Cypress.env("viewportHeight"));
+  
   cy.visit("/");
 });
 
-it("Should open the main page", () => {
-  cy.login("test@test.com", "test");
-  cy.contains("Добро пожаловать test@test.com").should("be.visible");
+it("Open the main page", () => {
+  cy.log("bropet@mail.ru", "123");
+  cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
 });
 
 it("Should not login with empty login", () => {
-  cy.login(" ", "test");
+  cy.log(" ", "123");
   cy.get("#mail")
     .then(($el) => $el[0].checkValidity())
     .should("be.false");
 });
 
 it("Should not login with empty password", () => {
-  cy.loginEmptyPass("test@test.com");
+  cy.loginEmptyPass("bropet@mail.ru", "");
   cy.get("#pass")
     .then(($el) => $el[0].checkValidity())
     .should("be.false");
 });
 
 it("Should log out", () => {
-  cy.login("test@test.com", "test");
-  cy.contains("Добро пожаловать test@test.com").should("be.visible");
+  cy.log("bropet@mail.ru", "123");
+  cy.contains("Добро пожаловать bropet@mail.ru").should("be.visible");
   cy.contains("Log out").click();
   cy.contains("Log in").should("be.visible");
 });
+})

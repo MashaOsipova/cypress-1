758c89ee4e06e8cad1076007d7819f9115dd1e02
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-05-11 12:28:35 +0300

    init

--- cypress/e2e/login.spec.js
+++ cypress/e2e/login.spec.js
@@ -1,37 +1,30 @@
-it("Should successfully login", () => {
-    cy.visit("/");
-    cy.login("test@test.com", "test");
-    cy.contains("Добро пожаловать test@test.com").should("be.visible");
-  });
+beforeEach(() => {
+  cy.viewport(Cypress.env("viewportWidth"), Cypress.env("viewportHeight"));
+  cy.visit("/");
+});
 
-  it("Should be able to logout", () => {
-    cy.logout();
-    cy.contains("Log in").should("be.visible");
-  });
+it("Should open the main page", () => {
+  cy.login("test@test.com", "test");
+  cy.contains("Добро пожаловать test@test.com").should("be.visible");
+});
 
-  it("Should not login with empty login", () => {
-    cy.visit("/");
-    cy.contains("Log in").click();
-    cy.get("#mail").type(" ");
-    cy.get("#pass").type("test");
-    cy.contains("Submit").click();
-    cy.get("#mail")
-      .then(($el) => $el[0].checkValidity())
-      .should("be.false");
-    cy.get("#mail")
-      .then(($el) => $el[0].validationMessage)
-      .should("contain", "Заполните это поле.");
-  });
-  
-  it("Should not login with empty password", () => {
-    cy.visit("/");
-    cy.contains("Log in").click();
-    cy.get("#mail").type("test@test.com");
-    cy.contains("Submit").click();
-    cy.get("#pass")
-      .then(($el) => $el[0].checkValidity())
-      .should("be.false");
-    cy.get("#pass")
-      .then(($el) => $el[0].validationMessage)
-      .should("contain", "Заполните это поле.");
-  });
+it("Should not login with empty login", () => {
+  cy.login(" ", "test");
+  cy.get("#mail")
+    .then(($el) => $el[0].checkValidity())
+    .should("be.false");
+});
+
+it("Should not login with empty password", () => {
+  cy.loginEmptyPass("test@test.com");
+  cy.get("#pass")
+    .then(($el) => $el[0].checkValidity())
+    .should("be.false");
+});
+
+it("Should log out", () => {
+  cy.login("test@test.com", "test");
+  cy.contains("Добро пожаловать test@test.com").should("be.visible");
+  cy.contains("Log out").click();
+  cy.contains("Log in").should("be.visible");
+});

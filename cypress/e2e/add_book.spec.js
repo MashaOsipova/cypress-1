9bdfb3360054835dc9599a05bf4928a6041d5ccb
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-05-11 12:29:17 +0300

    init

--- cypress/e2e/add_book.spec.js
+++ cypress/e2e/add_book.spec.js
@@ -1,40 +1,60 @@
-describe("When user add the books", () => {
-    it("Should be able to add a book", () => {
-        cy.visit("/");
-        cy.login("test@test.com", "test");
-        cy.typeForm(
-            "Eat, Pray, Love",
-            "One of the most iconic, beloved, and bestselling books of our time from the bestselling author of City of Girls and Big Magic, Elizabeth Gilbert.",
-            " Elizabeth Gilbert"
-        );
-        cy.contains("Submit").click();
-        cy.contains("Eat, Pray, Love");
+const bookOne = {
+    title: "Мертвые души",
+    description: "Приключение ",
+    author: "Николай Гоголь",
+  };
+  const bookTwo = {
+    title: "Роковой подарок",
+    description: "Детектив",
+    author: "Татьяна Устинова",
+  };
+  const bookThree = {
+    title: "Лето волонтера",
+    description: "Незамысловатая драмма",
+    author: "Сергей Лукьяненко",
+  };
+  const bookFour = {
+    title: "Война и Мир",
+    description: "Классика литературы",
+    author: "Лев Толстой",
+  };
+  
+  beforeEach(() => {
+    cy.viewport(Cypress.env("viewportWidth"), Cypress.env("viewportHeight"));
+    cy.visit("/");
+    cy.login("test@test.com", "test");
+  });
+  
+  describe("Favorite books testing", () => {
+    it("Should add book to favorite through function 'add new'", () => {
+      cy.addBook(bookOne);
+      cy.visit("/favorites");
+      cy.get(".card-title").should("contain.text", bookOne.title);
     });
-
-    it("Should not be able to add a book with empty form", () => {
-        cy.visit("/");
-        cy.login("test@test.com", "test");
-        cy.typeForm(" ", " ", " ");
-        cy.contains("Submit").click();
-        cy.get("#title")
-            .then(($el) => $el[0].checkValidity())
-            .should("be.false");
-        cy.get("#title")
-            .then(($el) => $el[0].validationMessage)
-            .should("contain", "Заполните это поле.");
+  
+    it("Should delete book from favorite", () => {
+      cy.visit("/favorites");
+      cy.contains(bookOne.title)
+        .should("be.visible")
+        .within(() => cy.get(".card-footer > .btn").click({ force: true }));
+      cy.contains(bookOne.title).should("not.exist");
     });
-
-    it("Should be able to add a book to favorite", () => {
-        cy.visit("/");
-        cy.login("test@test.com", "test");
-        cy.typeForm(
-            "Eat, Pray, Love",
-            "One of the most iconic, beloved, and bestselling books of our time from the bestselling author of City of Girls and Big Magic, Elizabeth Gilbert.",
-            " Elizabeth Gilbert");
-        cy.contains("Submit").click();
-        cy.contains("Eat, Pray, Love");
-        cy.get(".card-body").contains("Eat, Pray, Love").parent().siblings().children("button").click();
-        cy.contains("Favorites").click();
-        cy.contains("Eat, Pray, Love");
+  
+    it("Should add book to favorite through 'Book list' page", () => {
+      cy.addBookNoFavorite(bookTwo);
+      cy.contains(bookTwo.title)
+        .should("be.visible")
+        .within(() => cy.get(".card-footer > .btn").click({ force: true }));
+      cy.visit("/favorites");
+      cy.contains(bookTwo.title).should("be.visible");
     });
-});
+  
+    it("Should remove all favorite books", () => {
+      cy.addBook(bookThree);
+      cy.addBook(bookFour);
+      cy.removeAllFavorite();
+      cy.contains("Please add some book to favorit on home page!").should(
+        "exist"
+      );
+    });
+  });

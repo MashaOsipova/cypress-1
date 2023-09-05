91c47bd362f73fde1ef6dde016756afae216f74c
MashaOsipova <69155009+MashaOsipova@users.noreply.github.com>
2023-09-05 11:15:52 +0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,25 +1,23 @@
-
-beforeEach(() => {
-  cy.viewport(Cypress.env("viewportWidth"), Cypress.env("viewportHeight"));
-  cy.visit("/");
-});
-
-it("Should open the main page", () => {
-  cy.login("test@test.com", "test");
-  cy.contains("Добро пожаловать test@test.com").should("be.visible");
-});
-
-it("Should not login with empty login", () => {
-  cy.login(" ", "test");
-  cy.get("#mail")
-    .then(($el) => $el[0].checkValidity())
-    .should("be.false");
-});
-
-
-it("Should log out", () => {
-  cy.login("test@test.com", "test");
-  cy.contains("Добро пожаловать test@test.com").should("be.visible");
-  cy.contains("Log out").click();
-  cy.contains("Log in").should("be.visible");
-});
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  
+  it('Should open the main page', () => {
+    cy.contains('Books list');
+    cy.login('test@test.com', 'test');
+    cy.contains('Добро пожаловать test@test.com').should('be.visible');
+  });
+  
+  it('Should not login for empty login', () => {
+    cy.login('', 'test');
+    cy.get('#mail').then($el => $el[0].checkValidity()).should('be.false');
+    cy.get('#mail').then(($el) => $el[0].validationMessage).should('contain', 'Заполните это поле.');
+  });
+  
+  it('Should not login for empty password', () => {
+    cy.login('test@test.com');
+    cy.get('#pass').then(($el) => $el[0].checkValidity()).should('be.false');
+    cy.get('#pass').then(($el) => $el[0].validationMessage).should('contain', 'Заполните это поле.');
+  });
+})
